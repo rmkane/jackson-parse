9a2a9c8cfeaa24c90fbdbd05ed11065e53092d84
rmkane <rmkane@proton.me>
2025-11-20 18:53:49 -0500

    Fix controller content types

--- src/main/java/com/example/jacksonparse/controller/PersonController.java
+++ src/main/java/com/example/jacksonparse/controller/PersonController.java
@@ -4,13 +4,13 @@ import com.example.jacksonparse.model.Person;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +21,7 @@ public class PersonController {
     @Operation(
             summary = "Create or process a person",
             description = "Accepts and returns person data in JSON or XML format based on Content-Type and Accept headers",
-            requestBody = @RequestBody(
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
                     description = "Person object in JSON or XML format",
                     required = true,
                     content = {
@@ -40,13 +40,11 @@ public class PersonController {
                     )
             }
     )
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
-                              "application/json;charset=UTF-8", "application/xml;charset=UTF-8"},
-                 produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
-                             "application/json;charset=UTF-8", "application/xml;charset=UTF-8"})
-    public ResponseEntity<Person> createPerson(
-            @org.springframework.web.bind.annotation.RequestBody Person person,
-            HttpServletRequest request) {
+    @PostMapping(
+        consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<Person> createPerson(@RequestBody Person person, HttpServletRequest request) {
         String contentType = request.getContentType();
         String accept = request.getHeader("Accept");
         log.info("POST /api/person - Content-Type: {}, Accept: {}, Person[id={}, name={}]",
